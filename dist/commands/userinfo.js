5bdba93bade642ae7125b66855e256bdbb030ce0
b0463 <hkimpr40@gmail.com>
2022-05-22 03:16:27 -0300

    undefined feat(userinfo): user created acount date

--- dist/commands/userinfo.js
+++ dist/commands/userinfo.js
@@ -1,13 +1,45 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const discord_js_1 = require("discord.js");
+function getUserCreatedUTCDate(user) {
+    const now = user.createdAt;
+    let Day = now.getUTCDate();
+    let Month = now.getUTCMonth() + 1;
+    let Year = now.getUTCFullYear();
+    let Hour = now.getUTCHours();
+    let Minute = now.getUTCMinutes();
+    let Seconds = now.getUTCSeconds();
+    let ms = now.getUTCMilliseconds();
+    let FDay = `${Day}`;
+    let FMonth = `${Month}`;
+    let FYear = `${Year}`;
+    let FHour = `${Hour}`;
+    let FMinute = `${Minute}`;
+    let FSeconds = `${Seconds}`;
+    let Fms = `${ms}`;
+    if (Day < 10)
+        FDay = `0${Day}`;
+    if (Month < 10)
+        FMonth = `0${Month}`;
+    if (Hour < 10)
+        FHour = `0${Hour}`;
+    if (Minute < 10)
+        FMinute = `0${Minute}`;
+    if (Seconds < 10)
+        FSeconds = `0${Seconds}`;
+    if (ms < 10)
+        Fms = `00${ms}`;
+    if (ms < 100 && ms >= 10)
+        Fms = `0${ms}`;
+    const DateFormated = `UTC ${FDay}/${FMonth}/${FYear}\n${FHour}:${FMinute}:${FSeconds},${Fms}`;
+    return DateFormated;
+}
 function UserInfoEmbed(autor) {
-    console.log(autor.flags);
     return new discord_js_1.MessageEmbed()
         .setColor('#00ff00')
         .setTitle('UserInfo')
         .setAuthor({ name: autor.username })
         .setThumbnail(autor.avatarURL())
-        .addFields({ name: '**Tag**', value: `\`${autor.tag}\``, inline: true }, { name: '**ID**', value: `\`${autor.id}\``, inline: true }, { name: '**Flags**', value: `\`${autor.flags}\``, inline: true });
+        .addFields({ name: '**Tag**', value: `\`${autor.tag}\``, inline: true }, { name: '**ID**', value: `\`${autor.id}\``, inline: true }, { name: "**Conta criada em**", value: `\`${getUserCreatedUTCDate(autor)}\``, inline: true });
 }
 exports.default = {
     exec(msg, prefix) {
