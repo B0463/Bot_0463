4246a7f1dd138d048e2b8703e16db7840f4f5e54
b0463 <hkimpr40@gmail.com>
2022-05-22 06:10:14 -0300

    undefined chore(deployer): add deployer

---
+++ deploy/deployer.bat
@@ -0,0 +1,9 @@
+cd ..
+rd /s /q src
+del .gitignore
+del embed.txt
+del package-lock.json
+del package.json
+del README.md
+del tsconfig.json
+npm install discord.js
---
+++ deploy/deployer.sh
@@ -0,0 +1,9 @@
+cd ..
+rm -s src
+rm .gitignore
+rm embed.txt
+rm package-lock.json
+rm package.json
+rm README.md
+rm tsconfig.json
+npm install discord.js
